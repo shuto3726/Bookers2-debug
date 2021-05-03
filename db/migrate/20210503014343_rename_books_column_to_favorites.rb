5d3f98b5b33f1bbe4496b9ea907ff268e1670a51
shuto3726 <sss3726mmm@gmail.com>
2021-05-03 06:37:23 +0000

    いいね

---
+++ db/migrate/20210503014343_rename_books_column_to_favorites.rb
@@ -0,0 +1,4 @@
+class RenameBooksColumnToFavorites < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
---
+++ db/migrate/20210503014803_rename_books_id_column_to_favorites.rb
@@ -0,0 +1,4 @@
+class RenameBooksIdColumnToFavorites < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
