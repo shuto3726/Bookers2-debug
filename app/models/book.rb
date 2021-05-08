4dd6dbdc690f2d026898fdc8ef5e78d694c80c93
shuto3726 <sss3726mmm@gmail.com>
2021-05-08 09:01:12 +0000

    finish-5

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,8 +4,22 @@ class Book < ApplicationRecord
    	has_many :book_comments, dependent: :destroy
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
-	
+
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
+	end
+
+	def self.looks(search, word)
+		if search == "perfect_match"
+			@book = Book.where("title LIKE?","#{word}")
+		elsif search == "forward_match"
+			@book = Book.where("title LIKE?","#{word}%")
+		elsif search == "backword_match"
+			@book = Book.where("title LIKE?","%#{word}")
+		elsif search == "partial_match"
+			@book = Book.where("title LIKE?","%#{word}%")
+		else
+			@book = Bokk.all
+		end
 	end
 end
